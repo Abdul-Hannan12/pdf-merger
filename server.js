56c44e22be7a1c447f7be7098089bc3ce0d21836
Abdul Hannan <abdulhannan8540680@gmail.com>
2022-11-13 16:08:45 +0500

    got pdfs in request from frontend

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'})
+
 const port = 3000;
 
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, 'templates/index.html'));
+});
+
+app.post('/merge', upload.array('pdfs', 2), (req, res, next)=>{
+    console.log(req.files);
+    res.send({data: req.files});
 });
 
 app.listen(port, ()=>{
